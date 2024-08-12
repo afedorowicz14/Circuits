4cd93a9d4222492ae0a5fad7e886c7ec59065a6c
Aiden Fedorowicz <afedorowicz14@gmail.com>
2024-08-12 14:49:52 -0400

    hardening design

--- src/project.v
+++ src/project.v
@@ -16,10 +16,10 @@ module tt_um_afedorowicz14 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-reg [3:0] a;
-assign a = ui_in[7:4];
-reg [3:0] b;
-assign b = ui_in[3:0];
+reg [7:0] a;
+assign a = {4'b0000, ui_in[7:4]};
+reg [7:0] b;
+assign b = {4'b0000, ui_in[3:0]};
 reg [2:0] ALUOP;
 assign ALUOP = uio_in[2:0];
 reg [7:0] result;
